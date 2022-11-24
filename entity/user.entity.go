b2dc391406b0b5b0fabb4cd4a979a86498273ac5
torikki <kzymm1@gmail.com>
2022-11-25 01:03:55 +0300

    entity package created

---
+++ entity/user.entity.go
@@ -0,0 +1,16 @@
+package entity
+
+import "time"
+
+type User struct {
+	CreatedAt   time.Time `json:"created_at"`
+	DisplayName string    `json:"display_name"`
+	Email       string    `json:"email"`
+}
+
+type UserList  map[string]User
+
+type UserStore struct {
+	Increment int      `json:"increment"`
+	List      UserList `json:"list"`
+}
