30af09340abdc4d3d00854ff673e198128d54f0e
torikki <kzymm1@gmail.com>
2022-11-26 19:45:41 +0300

    handlers implemented

--- handler/v1/user.handler.go
+++ handler/v1/user.handler.go
@@ -3,6 +3,10 @@ package v1
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/torikki-tou/trueconf_testtask/common/response"
+	"github.com/torikki-tou/trueconf_testtask/dto"
 	"github.com/torikki-tou/trueconf_testtask/service"
 )
 
@@ -24,8 +28,74 @@ type userHandler struct {
 	userService service.UserService
 }
 
-func (h *userHandler) CreateUser(w http.ResponseWriter, r *http.Request) {}
-func (h *userHandler) GetUsers(w http.ResponseWriter, r *http.Request) {}
-func (h *userHandler) GetUserByID(w http.ResponseWriter, r *http.Request) {}
-func (h *userHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {}
-func (h *userHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {}
+func (h *userHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	request := dto.CreateUserRequest{}
+
+	if err := render.Bind(r, &request); err != nil {
+		_ = render.Render(w, r, response.ErrInvalidRequest(err))
+		return
+	}
+
+	user, err := h.userService.CreateUser(request)
+	if err != nil {
+		render.Render(w, r, response.ErrIternal(err))
+		return
+	}
+
+	render.Status(r, http.StatusCreated)
+	render.JSON(w, r, user)
+}
+
+func (h *userHandler) GetUsers(w http.ResponseWriter, r *http.Request) {
+	user, err := h.userService.GetUsers()
+	if err != nil {
+		render.Render(w, r, response.ErrIternal(err))
+		return
+	}
+
+	render.JSON(w, r, user)
+}
+
+func (h *userHandler) GetUserByID(w http.ResponseWriter, r *http.Request) {
+	userID := chi.URLParam(r, "id")
+
+	user, err := h.userService.GetUserByID(userID)
+	if err != nil {
+		render.Render(w, r, response.ErrIternal(err))
+		return
+	}
+
+	render.JSON(w, r, user)
+}
+
+func (h *userHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+	userID := chi.URLParam(r, "id")
+
+	request := dto.UpdateUserRequest{}
+
+	if err := render.Bind(r, &request); err != nil {
+		_ = render.Render(w, r, response.ErrInvalidRequest(err))
+		return
+	}
+
+	err := h.userService.UpdateUser(userID, request)
+	if err != nil {
+		render.Render(w, r, response.ErrIternal(err))
+		return
+	}
+
+	render.Status(r, http.StatusNoContent)
+}
+
+func (h *userHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	userID := chi.URLParam(r, "id")
+
+	err := h.userService.DeleteUser(userID)
+	if err != nil {
+		render.Render(w, r, response.ErrIternal(err))
+		return
+	}
+
+	render.Status(r, http.StatusNoContent)
+}
